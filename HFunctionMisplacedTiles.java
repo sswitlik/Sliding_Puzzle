3670384df2c24b959fe43d11267361327476896b
sswitlik <szczepan.switlik@gmail.com>
2017-05-04 01:10:34 +0200

    MisplacedTiles

--- HFunctionMisplacedTiles.java
+++ HFunctionMisplacedTiles.java
@@ -5,13 +5,19 @@ import sac.StateFunction;
 public class HFunctionMisplacedTiles extends StateFunction {
 
 	@Override
-    public double calculate (State state) {
-		Puzzle slidingPuzzle = ( Puzzle ) state;
+    public double calculate(State state) 
+	{
+		Puzzle puz = (Puzzle) state;
 		double h = 0.0;
-		for (int i = 0; i < slidingPuzzle .board.length; i++) {
-			if ((i != slidingPuzzle.emptyIndex ) && ( slidingPuzzle .board[i] != i))
-				h += 1.0;
+		
+		int k = 0;
+		for (int i= 0; i < puz.n; i++) {
+			for (int j = 0; j < puz.n; j++) {
+				if (puz.board[j][i] != k)
+					h += 1.0;
+				k++;
+			}
 		}
 		return h;
-    }
+	}
 }
